b13681720aa5b0292c976286887300764ca0c213
leeyongshik <jk1405@naver.com>
2022-09-28 09:28:28 +0900

    Test15552 / success

--- src/algorithm/Test15552.java
+++ src/algorithm/Test15552.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Test15552 {
 	public static void main(String[] args)throws IOException {
@@ -12,10 +13,14 @@ public class Test15552 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String s = br.readLine();
-		int a = Integer.parseInt(br.readLine());
+		StringTokenizer st1;
 		
-		
+		int t = Integer.parseInt(br.readLine());
+		for ( int i = 0; i<t; i++) {
+			st1 = new StringTokenizer(br.readLine());
+			bw.write((Integer.parseInt(st1.nextToken())+Integer.parseInt(st1.nextToken()))+"\n");
+		}
+		bw.close();
 		
 		
 		
