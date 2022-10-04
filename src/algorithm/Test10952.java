dac7256c4b2be0022c0a4fb2d6bd3991e2d333ab
leeyongshik <jk1405@naver.com>
2022-10-04 13:57:24 +0900

    test success

--- src/algorithm/Test10952.java
+++ src/algorithm/Test10952.java
@@ -1,9 +1,26 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test10952 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		ArrayList list = new ArrayList();
+		
+		while(true) {
+			int a = s.nextInt();
+			int b = s.nextInt();
+			
+			list.add(a+b);
+			
+			if (a ==0 && b==0) break;
+		}
+		
+		for(int i =0; i<list.size()-1;i++) {
+			System.out.println(list.get(i));
+		}
 
 	}
 
