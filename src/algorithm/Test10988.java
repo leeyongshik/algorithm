33ea6b4bd28497e39e74a64e3ca37c472697b920
jk1405 <jk1405@naver.com>
2023-06-12 09:22:39 +0900

    try

--- src/algorithm/Test10988.java
+++ src/algorithm/Test10988.java
@@ -3,13 +3,18 @@ package algorithm;
 import java.util.Scanner;
 
 public class Test10988 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		
 		String a = s.next();
-		
+		 int mid=(a.length()/2);
+	      int sw=1;
+	      for(int i=0;i<mid;i++) {
+	          if(a.charAt(i)!=a.charAt(a.length()-1-i)) {
+	              sw=0;
+	          }         
+          }
+      System.out.println(sw);
 	}
-
 }
