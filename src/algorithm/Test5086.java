afc6f905cf94330eb2b364f18c3cbdba2c31413b
jk1405 <jk1405@naver.com>
2023-08-09 09:20:56 +0900

    try

--- src/algorithm/Test5086.java
+++ src/algorithm/Test5086.java
@@ -4,7 +4,11 @@ public class Test5086 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		for (int i = 1; i <= 100; i++) {
+            if (i % 6 == 0 && i % 12 != 0) {
+                System.out.println(i);
+            }
+        }
 
 	} 
 }
