83d5c34eafa64a1095acc62792ddde712f96874d
jk1405 <jk1405@naver.com>
2023-08-29 09:27:47 +0900

    success

---
+++ src/algorithm/Test2501.java
@@ -0,0 +1,30 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Test2501 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		for(int i = 1; i <= a; i++) {
+			if( a % i == 0 ) {
+				list.add(i);
+			}
+		}
+		
+		if(b > list.size()) {
+			System.out.println(0);
+		}else {
+			System.out.println(list.get(b-1));
+		}
+
+	}
+
+}
