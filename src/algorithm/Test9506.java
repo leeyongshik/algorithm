24b05d90d96253662d03bc556a32724c51406eb6
jk1405 <jk1405@naver.com>
2023-12-15 14:19:21 +0900

    try

---
+++ src/algorithm/Test9506.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Test9506 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		
+		for(int a = 1; a<=2; a++) {
+			int b = s.nextInt();
+			
+			for(int c = 1; c<=b; c++) {
+				if(b / c == 0) {
+					System.out.println(c);
+				}
+			}
+		}
+	}
+
+}
+ 
